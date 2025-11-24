148d1644341bdb740e7b1f5815c899971b72985b
Koray Erdem <korayerdem5252@gmail.com>
2025-11-24 17:27:30 +0300

    Change Id type from Guid to int in BaseEntity

--- AMS/Backend/src/Core/AMS.Domain/Common/BaseEntity.cs
+++ AMS/Backend/src/Core/AMS.Domain/Common/BaseEntity.cs
@@ -2,7 +2,7 @@ namespace Namespace.Domain.Common
 {
     public abstract class BaseEntity : IEntity, IAuditableEntity, ISoftDeletable
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
+        public int Id { get; set; }
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public string? CreatedBy { get; set; }
